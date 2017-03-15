84829e7733bf41f16017734d0eacef16cd61e208
saurabh <zeayycool@gmail.com>
2017-03-16 00:10:28 +0530

    change in app and package file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path=require('path');
 const app =express();
-app.use(express.static('public')).listen(3000,(err)=>{
+const port =process.env.PORT||3000;
+app.use(express.static('public')).listen(port,(err)=>{
     if(err){
         console.error(err);
     }else{
-        console.log(`server is running on port 3000`);
+        console.log(`server is running on port ${port}`);
     }
 })
